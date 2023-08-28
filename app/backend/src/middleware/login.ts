a03f3d80f2b619eae666322c4bf10f781c8ee1be
Luar <luarrezende@gmail.com>
2023-08-28 15:33:59 -0300

    resolve requisito 10

--- app/backend/src/middleware/login.ts
+++ app/backend/src/middleware/login.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import validator from 'validator';
 
 function ValidationEmail(req: Request, res: Response, next: NextFunction): Response | void {
   const { email } = req.body;
@@ -6,8 +7,7 @@ function ValidationEmail(req: Request, res: Response, next: NextFunction): Respo
     return res.status(400).json({ message: 'All fields must be filled' });
   }
 
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  if (!emailRegex.test(email)) {
+  if (!validator.isEmail(email)) {
     return res.status(401).json({ message: 'Invalid email or password' });
   }
 
@@ -17,8 +17,12 @@ function ValidationEmail(req: Request, res: Response, next: NextFunction): Respo
 function ValidationPassword(req: Request, res: Response, next: NextFunction): Response | void {
   const { password } = req.body;
   const number = 7;
-  if (!password || password.length < number) {
+  if (!password || password === 0) {
     return res.status(400).json({ message: 'All fields must be filled' });
+  }
+
+  if (password.length < number) {
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
 
   next();
